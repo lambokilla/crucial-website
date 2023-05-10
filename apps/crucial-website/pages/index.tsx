edfeb32b6cb6122a3e6eed498756c6940cc342a0
Ben Scheib <lambo172@gmail.com>
2023-05-10 08:32:40 -0400

    -created css files -restructured css for index page -added animation to index page

--- apps/crucial-website/pages/index.tsx
+++ apps/crucial-website/pages/index.tsx
@@ -1,10 +1,38 @@
+import { motion, useScroll, useTransform } from "framer-motion";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import styles from '@/styles/Index.module.css';
 import Typed from "typed.js";
 
+// const { className, styles } = css.resolve`
+//     div {
+//         display: flex;
+//         flex-direction: column;
+//         align-items: center;
+//         justify-content: center;
+//         position: absolute;
+//         width: 390px;
+//         top: 50%;
+//         left: 50%;
+//         transform: translate(-50%, -50%);
+//     }
+// `;
+
 export function Index() {
+    const firstTargetRef = useRef<HTMLDivElement>(null);
     const mobileTextRef = useRef(null);
     const textRef = useRef(null);
 
+    const { scrollYProgress } = useScroll({
+        target: firstTargetRef,
+        offset: ["end end", "end start"],
+    });
+
+    const scale = useTransform(scrollYProgress, [0, 0.7], [1, 2]);
+    const y = useTransform(
+        scrollYProgress,
+        [0, .5, 1],
+        ["50%", "25%", "0%"]
+    );
 
     function IsMobile() {
 		const [size, setSize] = useState([0, 0]);
@@ -24,6 +52,10 @@ export function Index() {
 	}		
 
 	const mobile = IsMobile();
+
+    useEffect(() => {
+        console.log("yscroll", scrollYProgress);
+    }, [scrollYProgress]);
 
     useEffect(() => {
         console.log("mobile", mobile);
@@ -61,157 +93,29 @@ export function Index() {
     });
 
     return (
-        <div className="index">
-            <div className="container">
-                <section className="one page">
-                    <div className="text-type">
-                        <h1 className="text first-page-text">
-                            Your website, made <span className="text" ref={textRef}></span>
-                        </h1>
-                        <p className="text first-page-second" ref={mobileTextRef}></p>
-                    </div>
+        // <div className="index">
+            <div className={styles.container}>
+                <section ref={firstTargetRef} className={`${styles.one} ${styles.page}`}>
+                    <motion.div 
+                        style={{scale}}
+                        className={styles.textType}
+                    >
+                        <div className={styles.desktopText}>
+                            <h1 className={`${styles.text} ${styles.firstPageText}`}>
+                                Your website, made 
+                            </h1>
+                            <h1><span className={styles.text} ref={textRef}></span></h1>
+                        </div>
+                        <p className={`${styles.text} ${styles.firstPageSecond}`} ref={mobileTextRef}></p>
+                    </motion.div>
+                </section>
+                <section className={`${styles.two} ${styles.page}`}>
+                    <h1 className={styles.h1White}>Welcome to Crucial Web Solutions</h1>
+                    <h1 className={styles.h1White}>A new way to meet your website needs</h1>
                 </section>
                 
             </div>
-            <style jsx>{`
-
-        body::-webkit-scrollbar {
-            display:none;
-        }
-
-        .page {
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            scroll-snap-align: start;
-        }
-
-        .container {
-            scroll-snap-type: y mandatory;
-            overflow-y: scroll;
-            height: 100vh;
-            -webkit-overflow-scrolling: touch;
-            overflow: overlay;
-        }
-
-        .container::-webkit-scrollbar-track-piece:start {
-            background: transparent;
-            margin-top: 4.5rem;
-        }
-
-        .container::-webkit-scrollbar {
-            display: block;
-            width: 10px;
-            background: transparent;
-        }
-          
-        .container::-webkit-scrollbar-thumb {
-            background-color: #bababa;
-            border-radius: 15px;
-        } 
-
-
-        .index {
-            font-family: 'Source Sans Pro', sans-serif;
-            font-weight: lighter;
-            height: 100vh;
-        }
-
-        .one {
-            padding: 0;
-            margin: 0;
-            display:flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-            background-color: #2b323c;
-            overflow-x: hidden;
-            position: relative;
-        }
-
-        .text {
-            font-weight: 300;
-            color: #4070F4;
-            font-size: 3rem;
-        }
-
-        .first-page-text {
-            text-align: center;
-            color: #fff;
-            position: absolute;
-            top: -125px;
-        }
-
-        .first-page-second {
-            position: absolute;
-            top: 25px;
-        }
-
-        .first-page-text span {
-            display: none;
-        }
-
-        .text-type {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            position: absolute;
-            width: 390px;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-
-        }
-
-        @media (min-width: 32rem) {
-            .first-page-text {
-                top: -75px;
-            }
-
-            .first-page-second {
-                top: 10px;
-            }
-
-            .text-type {
-                width: 100vw;
-            }
-        }
-
-        @media (min-width: 40rem) {
-            .first-page-text {
-                left: 0;
-            }
-
-            .first-page-text span {
-                display: inline;
-            }
-
-            .first-page-second {
-                display: none;
-            }
-
-            .text-type {
-                width: 620px;
-                flex-direction: row;
-            }
-        }
-
-        @media (min-width: 40rem) {
-            @media(min-height: 31rem) {
-                .container::-webkit-scrollbar-track-piece:start {
-                    background: transparent;
-                    margin-top: 3.5rem;
-                }
-
-                .container::-webkit-scrollbar-thumb {
-                    border: .25px solid #7f7f7f;
-                } 
-            }
-        }
-      `}</style>
-        </div>
+        // </div>
     );
 }
 
