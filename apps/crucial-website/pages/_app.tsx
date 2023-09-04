c888440e51e3b79825b363da75ffc50a5b4c9b42
Ben <lambo172@gmail.com>
2023-09-04 07:21:53 -0400

    -first push to branch

--- apps/crucial-website/pages/_app.tsx
+++ apps/crucial-website/pages/_app.tsx
@@ -9,25 +9,25 @@ function CustomApp({ Component, pageProps }: AppProps) {
 
 
 	const toggleButton = () => {
-        const mobileNav = document.querySelector(".mobile-nav");
-        const backdrop = document.querySelector(".backdrop");
+		const mobileNav = document.querySelector(".mobile-nav");
+		const backdrop = document.querySelector(".backdrop");
 
-        mobileNav?.classList.add("open");
-        backdrop?.classList.add("open");
-        backdrop?.classList.add("opacity");
-    }
+		mobileNav?.classList.add("open");
+		backdrop?.classList.add("open");
+		backdrop?.classList.add("opacity");
+	}
 
-    const closeMobileNav = () => {
-        const mobileNav = document.querySelector(".mobile-nav");
-        const backdrop = document.querySelector(".backdrop");
-        mobileNav?.classList.remove("open");
-        backdrop?.classList.remove("open");
-        backdrop?.classList.remove("opacity");
-    }
-  	return (
+	const closeMobileNav = () => {
+		const mobileNav = document.querySelector(".mobile-nav");
+		const backdrop = document.querySelector(".backdrop");
+		mobileNav?.classList.remove("open");
+		backdrop?.classList.remove("open");
+		backdrop?.classList.remove("opacity");
+	}
+	return (
 		<>
 			<Head>
-				<title>Welcome to my-site!</title>
+				<title>Crucial Web Solutions</title>
 			</Head>
 			<div className="backdrop" onClick={() => closeMobileNav()}></div>
 			<header className="main-header">
@@ -41,22 +41,27 @@ function CustomApp({ Component, pageProps }: AppProps) {
 				<nav className="main-nav">
 					<ul className="main-nav__items">
 						<li className="main-nav__item">
-							<Link href="/" style={{textDecoration: 'none'}}>
+							<Link href="/" style={{ textDecoration: 'none' }}>
 								<p className='main-nav__link'>Home</p>
 							</Link>
 						</li>
 						<li className="main-nav__item">
-							<Link href="/plans" style={{textDecoration: 'none'}}>
-								<p className='main-nav__link'>Plans</p>
-							</Link>
-						</li>
-                        <li className="main-nav__item">
-							<Link href="/about" style={{textDecoration: 'none'}}>
+							<Link href="/about" style={{ textDecoration: 'none' }}>
 								<p className='main-nav__link'>About Us</p>
 							</Link>
 						</li>
-                        <li className="main-nav__item">
-							<Link href="/contact" style={{textDecoration: 'none'}}>
+						<li className="main-nav__item">
+							<Link href="/services" style={{ textDecoration: 'none' }}>
+								<p className='main-nav__link'>Services</p>
+							</Link>
+						</li>
+						<li className="main-nav__item">
+							<Link href="/portfolio" style={{ textDecoration: 'none' }}>
+								<p className='main-nav__link'>Portfolio</p>
+							</Link>
+						</li>
+						<li className="main-nav__item">
+							<Link href="/contact" style={{ textDecoration: 'none' }}>
 								<p className='main-nav__link'>Contact Us</p>
 							</Link>
 						</li>
@@ -66,22 +71,22 @@ function CustomApp({ Component, pageProps }: AppProps) {
 			<nav className="mobile-nav">
 				<ul className="mobile-nav__items">
 					<li className="mobile-nav__item">
-						<Link href="/" style={{textDecoration: 'none'}}>
+						<Link href="/" style={{ textDecoration: 'none' }}>
 							<p className='mobile-nav__link' onClick={() => closeMobileNav()}>Home</p>
 						</Link>
 					</li>
 					<li className="mobile-nav__item">
-						<Link href="/plans" style={{textDecoration: 'none'}}>
+						<Link href="/plans" style={{ textDecoration: 'none' }}>
 							<p className='mobile-nav__link' onClick={() => closeMobileNav()}>Plans</p>
 						</Link>
 					</li>
-                    <li className="mobile-nav__item">
-						<Link href="/about" style={{textDecoration: 'none'}}>
+					<li className="mobile-nav__item">
+						<Link href="/about" style={{ textDecoration: 'none' }}>
 							<p className='mobile-nav__link' onClick={() => closeMobileNav()}>About Us</p>
 						</Link>
 					</li>
-                    <li className="mobile-nav__item">
-						<Link href="/contact" style={{textDecoration: 'none'}}>
+					<li className="mobile-nav__item">
+						<Link href="/contact" style={{ textDecoration: 'none' }}>
 							<p className='mobile-nav__link' onClick={() => closeMobileNav()}>Contact Us</p>
 						</Link>
 					</li>
@@ -95,7 +100,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
 				
         	`}</style>
 		</>
-  );
+	);
 }
 
 export default CustomApp;
