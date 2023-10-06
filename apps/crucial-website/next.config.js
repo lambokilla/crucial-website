d1212792d13e5fc2d7fd8c16ea7054e31bc3bbf3
Ben Scheib <lambo172@gmail.com>
2023-10-06 12:58:00 -0400

    -update

--- apps/crucial-website/next.config.js
+++ apps/crucial-website/next.config.js
@@ -14,8 +14,4 @@ const nextConfig = {
   },
 };
 
-module.exports = {
-  images: {
-    domains: ['crucialwebsitestack-imagebucket6194e37c-105gw6s2x4ta3.s3.us-east-1.amazonaws.com']
-  }
-}
+module.exports = withNx(nextConfig);
