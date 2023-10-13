246e47b1719d04f6644a03ebfc09049ee123b95a
Ben Scheib <lambo172@gmail.com>
2023-10-13 11:49:30 -0400

    -updated mail address

--- apps/crucial-website/pages/components/Footer.tsx
+++ apps/crucial-website/pages/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className={styles.container}>
             <p className={styles.pText}>Crucial Web Solutions</p>
             <p className={styles.pTextContact}>Contact Us:</p>
-            <a className={styles.link} href="mailto:info@crucialrepair.com">bscheib@crucialwebsolutions.com</a>
+            <a className={styles.link} href="mailto:bscheib@crucialwebsolutions.com">bscheib@crucialwebsolutions.com</a>
             <a className={styles.link} href="tel:+1513-348-7019">513-348-7019</a>
         </div>
     )
